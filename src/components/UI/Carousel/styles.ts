ae33f66d8ea1c2d909c5650e2736f4d20757d1f8
Ruan Lucena <100430135+SrLuc@users.noreply.github.com>
2024-05-23 14:19:41 -0300

    refactor: Card Styles

--- src/components/UI/Carousel/styles.ts
+++ src/components/UI/Carousel/styles.ts
@@ -1,15 +1,25 @@
 import styled from "styled-components";
 import { softexTheme } from "../../../css/themes/softexTheme";
+import {reposivityQuery} from "../../../css/themes/reposivityQuery"
 
 export const StyledCarouselItem = styled.section`
     margin-left: ${softexTheme.spacing.large};
     padding: ${softexTheme.spacing.medium};
 
-
   img {
     border-radius: ${softexTheme.borderRadius.large};
     width: 100%;
     height: 250px;
+
+    ${reposivityQuery.tablet} {
+      height: 200px;
+      width: 250px;
+    }
+
+    ${reposivityQuery.mobile} {
+      height: 200px;
+      width: 230px;
+    }
   }
 
   h1 {
